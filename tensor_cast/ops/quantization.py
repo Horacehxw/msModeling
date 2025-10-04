419869e7fbd6fb3568279a298f1aaa457f8f146f
jgong5 <steven.gong@gmail.com>
2025-10-04 13:24:42 +0800

    This PR removes the dynamic_quant_linear op and split it into dynamic quant and static linear ops. It also adds the fusion of RMSNorm ops with dynamic quant ops.
    Co-authored-by: Jiong Gong<steven.gong@gmail.com>

--- tensor_cast/ops/quantization.py
+++ tensor_cast/ops/quantization.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional, Tuple
 
 import torch
 
@@ -14,3 +14,64 @@ def _quantize(
 ) -> torch.Tensor:
     """`out = clamp(round(x / scale) + offset)`"""
     return torch.empty_like(x, dtype=out_dtype)
+
+
+@register_tensor_cast_op("dynamic_quantize_asymmetric")
+def _dynamic_quantize_asymmetric(
+    x: torch.Tensor,
+    dims: List[int],
+    scale_dtype: torch.dtype = torch.float32,
+    out_dtype: torch.dtype = torch.int8,
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
+    Dynamically quantize the input tensor `x` to `out_dtype` with symmetric quantization.
+    The quantization scale is computed based on the absolute max value of `x` along the specified dimensions.
+
+    Args:
+        x: The input tensor to be quantized.
+        dims: The dimensions along which to compute the quantization scale.
+        scale_dtype: The data type for the quantization scale (default: torch.float32).
+        out_dtype: The target data type for quantization (default: torch.int8).
+
+    Returns:
+        A tuple containing:
+        - The quantized tensor.
+        - The quantization scale tensor. When `dims` is empty, the scale is a scalar tensor.
+          Otherwise, the scale tensor has the same shape as `x` with the specified `dims` reduced to size 1.
+        - The quantization offset tensor, the same shape as the scale tensor but with dtype torch.int32.
+    """
+    if len(dims) == 0:
+        scale_shape = torch.Size([])
+    else:
+        scale_shape = list(x.shape)
+        for dim in dims:
+            scale_shape[dim] = 1
+        scale_shape = torch.Size(scale_shape)
+    return (
+        torch.empty_like(x, dtype=out_dtype),
+        torch.empty(scale_shape, dtype=scale_dtype, device="meta"),
+        torch.empty(scale_shape, dtype=torch.int32, device="meta"),
+    )
+
+
+@register_tensor_cast_op("dynamic_quantize_symmetric")
+def _dynamic_quantize_symmetric(
+    x: torch.Tensor,
+    dims: List[int],
+    scale_dtype: torch.dtype = torch.float32,
+    out_dtype: torch.dtype = torch.int8,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Same as `dynamic_quantize_asymmetric` but for symmetric quantization (no offset).
+    """
+    if len(dims) == 0:
+        scale_shape = torch.Size([])
+    else:
+        scale_shape = list(x.shape)
+        for dim in dims:
+            scale_shape[dim] = 1
+        scale_shape = torch.Size(scale_shape)
+    return (
+        torch.empty_like(x, dtype=out_dtype),
+        torch.empty(scale_shape, dtype=scale_dtype, device="meta"),
+    )
