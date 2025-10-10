4430c9ca4cad320a803e5f6994a6d463e5deae95
zhengxinqian <qianzhengxin@huawei.com>
2025-10-10 10:00:42 +0800

    【feature】仿真建模工具添加profiling能力
    Co-authored-by: zhengxinqian<qianzhengxin@huawei.com>

--- service_sim/engine.py
+++ service_sim/engine.py
@@ -1,17 +1,18 @@
 # Copyright Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
-import threading
 from typing import Dict, List
 
 import stime
 from service_sim.device import Device
-from service_sim.model_runner import ModelRunner, ModelConfig
-from service_sim.request import Request, RequestState
 from service_sim.kv_cache_manager import KVCacheManager
+from service_sim.model_runner import ModelConfig, ModelRunner
+from service_sim.request import Request, RequestState
+from service_sim.profiler import profiler_interface
+from service_sim.config import Config
 
 
 logger = stime.get_logger(__name__)
 
-MEM_LEFT_FOR_KV_CACHE = 40 * 1024 * 1024 # 40GB
+MEM_LEFT_FOR_KV_CACHE = 40 * 1024 * 1024  # 40GB
 MAX_TOKENS_BUDGET = 16384
 
 
@@ -56,17 +57,22 @@ class BatchScheduler(stime.Task):
                 raise ValueError(
                     f"num_computed_tokens should be positive, got {num_computed_tokens}"
                 )
-            
+
             # try to allocate KV cache for the request
             while True:
-                new_blocks = self.kv_manager.allocate_slots(request.id, num_computed_tokens)
+                new_blocks = self.kv_manager.allocate_slots(
+                    request.id, num_computed_tokens
+                )
 
                 if new_blocks is None:
                     # The request cannot be scheduled. Preempt the lowest-priority request. now naively preempt the last
                     request_to_preempt = self.running_queue[-1]
-                    if request_to_preempt is not request: # not the current req
+                    if request_to_preempt is not request:  # not the current req
                         self._process_preempted_request(request_to_preempt)
-                        logger.debug("BatchScheduler._schedule: preempt request %s", request_to_preempt.id)
+                        logger.debug(
+                            "BatchScheduler._schedule: preempt request %s",
+                            request_to_preempt.id,
+                        )
                         preempt_reqs.append(request_to_preempt)
                     else:
                         can_schedule = False
@@ -77,7 +83,9 @@ class BatchScheduler(stime.Task):
             if not can_schedule:
                 break
             if new_blocks is None:
-                raise ValueError("BatchScheduler._schedule failed: new_blocks should not be None")
+                raise ValueError(
+                    "BatchScheduler._schedule failed: new_blocks should not be None"
+                )
 
             # kv cache allocation success, schedule the request
             token_budget -= num_computed_tokens
@@ -91,20 +99,31 @@ class BatchScheduler(stime.Task):
             while self.waiting_queue and token_budget > 0:
                 request = self.waiting_queue[0]
                 # check if need receive kv transfer, if need then try to receive, if failed skip
-                if request.state == RequestState.KVS_TRANSFERRING and request.need_kv_transfer and \
-                    not request.kv_transfer_done:
+                if (
+                    request.state == RequestState.KVS_TRANSFERRING
+                    and request.need_kv_transfer
+                    and not request.kv_transfer_done
+                ):
                     if not self._receive_remote_kvs(request):
                         continue
-                num_computed_tokens = min(token_budget, request.num_current_max_new_tokens)
+                num_computed_tokens = min(
+                    token_budget, request.num_current_max_new_tokens
+                )
                 if num_computed_tokens <= 0:
                     raise ValueError(
                         f"num_computed_tokens should be positive, got {num_computed_tokens}"
                     )
-                new_blocks = self.kv_manager.allocate_slots(request.id, num_computed_tokens)
+                new_blocks = self.kv_manager.allocate_slots(
+                    request.id, num_computed_tokens
+                )
                 # try to allocate kv cache, if failed, no need to check the rest requests in waiting queue
                 if new_blocks is None:
-                    logger.debug("BatchScheduler._schedule: Schedule request %s failed, due to lack of KV cache. " \
-                        "KV manager status: %s", request, self.kv_manager.stats())
+                    logger.debug(
+                        "BatchScheduler._schedule: Schedule request %s failed, due to lack of KV cache. "
+                        "KV manager status: %s",
+                        request,
+                        self.kv_manager.stats(),
+                    )
                     break
 
                 # kv cache allocation success, schedule the request
@@ -116,7 +135,9 @@ class BatchScheduler(stime.Task):
                 self.running_queue.append(request)
 
         while len(self.running_queue) == 0 and len(self.waiting_queue) == 0:
-            logger.debug("BatchScheduler._schedule: no requests are scheduled, passivate current BatchScheduler")
+            logger.debug(
+                "BatchScheduler._schedule: no requests are scheduled, passivate current BatchScheduler"
+            )
             self.wait()
 
     def _receive_remote_kvs(self, request) -> bool:
@@ -142,29 +163,68 @@ class BatchScheduler(stime.Task):
         self.running_queue.remove(request)
         self.waiting_queue = [request] + self.waiting_queue
         request.state = RequestState.RECOMPUTATION
-        request.num_current_max_new_tokens = request.num_input_tokens + request.num_decoded_tokens
+        request.num_current_max_new_tokens = (
+            request.num_input_tokens + request.num_decoded_tokens
+        )
         request.seq_len = 0
         request.query_len = 0
         self.kv_manager.free(request.id)
         logger.debug("Request %d is done preempting", request.id)
 
+    def _process_finished_request(self, request: Request):
+        self.kv_manager.free(request.id)
+        self.running_queue.remove(request)
+        request.state = RequestState.DECODE_DONE
+
     def _scheduling_loop(self):
         """
-        Threading target: 
+        Threading target:
         First, schedule the requests into waiting_queue or running_queue.
         Second, execute the requests in the running_queue.
         """
         try:
             while True:
                 logger.debug("in schedule   ")
+                if profiler_interface.is_profiling_ready() and Config.get_instance().enable_profiling:
+                    prof = (
+                        profiler_interface.SimProfiler(profiler_interface.Level.INFO)
+                        .domain("BatchSchedule")
+                        .span_start("batchFrameworkProcessing")
+                    )
+                    before_running_queue = self.running_queue
+                    before_waiting_queue = self.waiting_queue
                 self._schedule()
+                if profiler_interface.is_profiling_ready() and Config.get_instance().enable_profiling:
+                    request_id_with_iter_list = profiler_interface.get_iter_size_info(
+                        self.running_queue, increase_iter_size=True)
+
+                    if len(request_id_with_iter_list) != 0:
+                        profiler_interface.queue_profiler(before_running_queue, self.running_queue, "running")
+                        profiler_interface.queue_profiler(before_waiting_queue, self.waiting_queue, "waiting")
+                        prof.res(request_id_with_iter_list)
+
+                        batch_type = profiler_interface.get_batch_type(request_id_with_iter_list)
+                        prof.attr("batch_type", batch_type)
+                        prof.span_end()
                 if len(self.running_queue) != 0:
-                    logger.debug(f"Scheduled batch size: {len(self.running_queue)}"
-                        f"request ids: {[request.id for request in self.running_queue]}")
+                    logger.debug(
+                        f"Scheduled batch size: {len(self.running_queue)}"
+                        f"request ids: {[request.id for request in self.running_queue]}"
+                    )
+                    if profiler_interface.is_profiling_ready() and Config.get_instance().enable_profiling:
+                        if request_id_with_iter_list:
+                            prof = profiler_interface.SimProfiler(profiler_interface.Level.INFO).domain("ModelExecute")
+                            prof.res(request_id_with_iter_list)
+                            prof.attr("batch_type", batch_type)
+                            prof.span_start("modelExec")
+                            prof.attr("batch_size", len(self.running_queue))
                     self.model_runner.process_batch(self.running_queue)
+                    if profiler_interface.is_profiling_ready() and Config.get_instance().enable_profiling:
+                        if request_id_with_iter_list:
+                            prof.span_end()
                     self._postprocess_batch()
         except:
-            logger.error(f"Unexpected exception in the scheduling loop", exc_info=True)
+            logger.error("Unexpected exception in the scheduling loop", exc_info=True)
             raise
 
     def _postprocess_batch(self):
@@ -175,27 +235,33 @@ class BatchScheduler(stime.Task):
         idx = 0
         while idx < len(self.running_queue):
             request = self.running_queue[idx]
-            if request.state not in [RequestState.PREFILLING, RequestState.DECODING, RequestState.RECOMPUTATION]:
-                raise ValueError("In _postprocess_batch, request.state should be PREFILLING, DECODING or " \
-                    "RECOMPUTATION, but get %s" % request.state)
+            if request.state not in [
+                RequestState.PREFILLING,
+                RequestState.DECODING,
+                RequestState.RECOMPUTATION,
+            ]:
+                raise ValueError(
+                    "In _postprocess_batch, request.state should be PREFILLING, DECODING or "
+                    "RECOMPUTATION, but get %s" % request.state
+                )
             if request.num_current_max_new_tokens == 0:
                 # totally finish one step of prefilling or decoding
                 request.num_decoded_tokens += 1
                 if request.num_decoded_tokens >= request.num_output_tokens:
+                    self._process_finished_request(request)
                     self.requests.pop(request.id)
-                    self.kv_manager.free(request.id)
-                    self.running_queue.remove(request)
-                    request.state = RequestState.DECODE_DONE
                     continue
 
                 request.num_current_max_new_tokens = 1
                 if request.state == RequestState.PREFILLING:
                     request.state = RequestState.PREFILL_DONE
-                    
+
                     if request.need_kv_transfer:
                         if request.kv_transfer_done:
-                            raise ValueError("BatchScheduler._postprocess_batch failed: "
-                                "request's kv cache should not been transferred")
+                            raise ValueError(
+                                "BatchScheduler._postprocess_batch failed: "
+                                "request's kv cache should not been transferred"
+                            )
                         self._send_kvs_from_remote(request)
                         self.requests.pop(request.id)
                         continue
@@ -205,10 +271,13 @@ class BatchScheduler(stime.Task):
 
             else:
                 # case when prefill are chunked, nothing need to do right now
-                logger.debug("requset %d are chunked, num of tokens need to compute left %d", request.id, \
-                    request.num_current_max_new_tokens)
+                logger.debug(
+                    "requset %d are chunked, num of tokens need to compute left %d",
+                    request.id,
+                    request.num_current_max_new_tokens,
+                )
             idx += 1
-    
+
 
 class Engine:
     """
@@ -227,7 +296,9 @@ class Engine:
         precision_bytes = model_config.kwargs.get("precision_bytes")
         num_layers = model_config.kwargs.get("num_layers")
         if None in [head_dim, num_heads, precision_bytes, num_layers]:
-            raise ValueError("head_dim, num_heads, precision_bytes and num_layers must be set")
+            raise ValueError(
+                "head_dim, num_heads, precision_bytes and num_layers must be set"
+            )
         kv_cache_per_layer = MEM_LEFT_FOR_KV_CACHE * len(devices) // num_layers
         block_nums = kv_cache_per_layer // (2 * head_dim * num_heads * precision_bytes)
         kv_manager = KVCacheManager(block_nums)
@@ -235,9 +306,15 @@ class Engine:
 
     def handle(self, request: Request):
         logger.debug(f"Engine handling {request}")
-        if request.state not in [RequestState.PREFILLING, RequestState.DECODING, RequestState.KVS_TRANSFERRING]:
-            raise ValueError("Engine.handle failed, request.state should be PREFILLING, DECODING or "
-                "KVS_TRANSFERRING but get request.state: %s" % request.state)
+        if request.state not in [
+            RequestState.PREFILLING,
+            RequestState.DECODING,
+            RequestState.KVS_TRANSFERRING,
+        ]:
+            raise ValueError(
+                "Engine.handle failed, request.state should be PREFILLING, DECODING or "
+                "KVS_TRANSFERRING but get request.state: %s" % request.state
+            )
         self.batch_scheduler.add(request)
 
     def get_work_load(self) -> int:
