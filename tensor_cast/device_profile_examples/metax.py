d112041b26b64eb297f6e0b09ce34b11acbc3462
Jiong Gong <steven.gong@gmail.com>
2025-10-18 17:33:35 +0800

    Add files left-over.

---
+++ tensor_cast/device_profile_examples/metax.py
@@ -0,0 +1,46 @@
+import torch
+
+from ..utils import DTYPE_FP4, DTYPE_FP8
+from ..device import CommGrid, DeviceProfile, InterconnectTopology, StaticCost
+
+
+
+class METAX:
+    STATIC_COST = StaticCost(mma_op_cost_s=5 * 1e-6, gp_op_cost_s=2 * 1e-6)
+
+    # TODO(jgong5): double-confirm latency
+    # TODO(jgong5): double-confirm communication efficiency
+    INTERCONNECT = CommGrid(
+        grid=torch.arange(8 * 8).reshape(8, 8),
+        topologies={
+            0: InterconnectTopology(
+                bandwidth_bytes_ps=12.5 * 1e9, latency_s=10 * 1e-6, comm_efficiency=0.7
+            ),
+            1: InterconnectTopology(
+                bandwidth_bytes_ps=448 * 1e9, latency_s=1 * 1e-6, comm_efficiency=0.7
+            ),
+        },
+    )
+
+    C550 = DeviceProfile(
+        name="C550",
+        vendor="METAX",
+        mma_ops={
+            torch.float32: 140 * 1e12,
+            torch.bfloat16: 280 * 1e12,
+            torch.half: 280 * 1e12,
+            torch.int8: 560 * 1e12,
+        },
+        gp_ops={
+            torch.float32: 54 * 1e12,
+            torch.bfloat16: 54 * 1e12,
+            torch.half: 108 * 1e12,
+        },
+        memory_size_bytes=64 * (1024**3),
+        memory_bandwidth_bytes_ps=1.6 * (1024**4),
+        # The efficiencies are something we need to calibrate
+        compute_efficiency=0.7,
+        memory_efficiency=0.6,
+        comm_grid=INTERCONNECT,
+        static_cost=STATIC_COST,
+    )
