901ea9ab515d819d9e6e70ff512b3eecc2c168df
jgong5 <jgong5@noreply.gitcode.com>
2025-11-19 19:14:22 +0800

    This PR supports MTP for arbitrary transformer models by recognizing the last decoder layer as the MTP layer.
    Co-authored-by: g00929853<gongjiong@huawei.com>
    Co-authored-by: Jiong Gong<steven.gong@gmail.com>

--- tensor_cast/tests/test_mtp.py
+++ tensor_cast/tests/test_mtp.py
@@ -4,6 +4,7 @@ import torch
 from parameterized import parameterized
 
 from ..compilation import get_backend
+from ..layers.attention import AttentionTensorCast
 from ..layers.mla import MultiheadLatentAttentionTensorCast
 from ..layers.sampler import SamplingMetadata
 from ..model_config import (
@@ -17,7 +18,11 @@ from ..patch_torch import patch_torch
 from ..transformers.model import TransformerModel
 from ..transformers.utils import model_id_to_json, model_id_to_mtp_block_module_name
 
-from .test_common import create_mla_metadata_and_kv_cache, has_submodule_with_cls_name
+from .test_common import (
+    create_attn_metadata_and_kv_cache,
+    create_mla_metadata_and_kv_cache,
+    has_submodule_with_cls_name,
+)
 
 
 class MtpTestCase(unittest.TestCase):
@@ -172,6 +177,49 @@ class MtpTestCase(unittest.TestCase):
         # make sure all original attention modules have been replaced
         self.assertTrue(
             has_submodule_with_cls_name(model, "MultiheadLatentAttentionTensorCast")
+        )
+        inputs = torch.empty([1, num_tokens], dtype=torch.long, device="meta")
+        position_ids = torch.empty([1, num_tokens], dtype=torch.long, device="meta")
+        with torch.no_grad(), patch_torch():
+            outputs = model.forward(
+                inputs,
+                position_ids,
+                attention_meta=attn_meta,
+                kv_cache_by_layers=kv_cache_by_layers,
+                sampling_metadata=SamplingMetadata(
+                    query_start_loc=attn_meta.query_start_loc,
+                    selected_token_indices=None,
+                ),
+            )
+            self.assertEqual(outputs.shape, (2, num_mtp_layers + 1))
+
+    @parameterized.expand(
+        [
+            ["Qwen/Qwen3-32B", False],
+            ["Qwen/Qwen3-32B", True],
+            ["Qwen/Qwen3-235B-A22B", False],
+            ["zai-org/GLM-4.5", False],
+        ]
+    )
+    def test_automatic_mtp_mode(self, model_id, do_compile):
+        model_config = ModelConfig(
+            ParallelConfig(),
+            QuantConfig(),
+            attention_cls=AttentionTensorCast,
+            enable_repetition=True,
+        )
+        num_mtp_layers = 3
+        mtp_config = MtpConfig(
+            num_mtp_layers=num_mtp_layers,
+        )
+        model_config.mtp_config = mtp_config
+        model = TransformerModel(model_id, model_config)
+        if do_compile:
+            model = torch.compile(
+                model, backend=get_backend(), dynamic=True, fullgraph=True
+            )
+        attn_meta, kv_cache_by_layers, num_tokens = create_attn_metadata_and_kv_cache(
+            model, model_config
         )
         inputs = torch.empty([1, num_tokens], dtype=torch.long, device="meta")
         position_ids = torch.empty([1, num_tokens], dtype=torch.long, device="meta")
